d7c95a45f0c1d52cdd6584e5b36d004b6ce677e0
blackbeard334 <blackbeard334@users.noreply.github.com>
2019-08-07 19:36:22 +0200

    Bla

---
+++ src/main/java/Bla.java
@@ -0,0 +1,30 @@
+import annotation.OperatorOverloading;
+
+@OperatorOverloading
+public class Bla {
+    int x;
+
+    //savatage
+    Bla operator+(Bla in) {
+        x += in.x;
+        return this;
+    }
+//
+//    Bla operator-(Bla in) {
+//        x -= in.x;
+//        return this;
+//    }
+//
+//    Bla operator*(Bla in) {
+//        x *= in.x;
+//        return this;
+//    }
+//
+//    Bla operator/(Bla in) {
+//        x /= in.x;
+//        return this;
+//    }
+//    /*
+//    METAL
+//    */
+}
